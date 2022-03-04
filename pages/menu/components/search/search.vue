20b6f5b5a1f4deb9d39277eefb29cc6f40b3c20b
coderyh <2368009927@qq.com>
2022-03-04 17:35:15 +0800

    搜索

--- pages/menu/components/search/search.vue
+++ pages/menu/components/search/search.vue
@@ -131,17 +131,21 @@ export default {
 			},
 			handleKeywordInput(e) {
         function debounce(fn, delay) {
-          let timer = null;
+          // 1.定义一个定时器, 保存上一次的定时器
+          let timer = null
 
-          return function () {
-            clearTimeout(timer);
-            let context = this;
-            let args = arguments;
-
-            timer = setTimeout(function () {
-              fn.apply(context, args);
-            }, delay);
+          // 2.真正执行的函数
+          const _debounce = function() {
+            // 取消上一次的定时器
+            if (timer) clearTimeout(timer)
+            // 延迟执行
+            timer = setTimeout(() => {
+              // 外部传入的真正要执行的函数
+              fn()
+            }, delay)
           }
+
+          return _debounce
         }
         let debounceShowLog = debounce(this.custom, 2000);
         debounceShowLog()
