326ab45938bebb927a0092c50aceb1c1fd35a48f
coderyh <2368009927@qq.com>
2022-04-14 15:19:13 +0800

    正餐

--- App.vue
+++ App.vue
@@ -4,6 +4,18 @@ export default {
     console.log('从二维码中取的数据', options)
     const appId = uni.getAccountInfoSync().miniProgram.appId;
     uni.setStorageSync('appid', appId);
+    uni.setStorageSync('syyid', '00268');
+    uni.setStorageSync('vipid', '26512220');
+    uni.setStorageSync('posid', '80800101');
+    uni.setStorageSync('tableid', '4');
+    uni.setStorageSync('tablenumber', '1');
+    uni.setStorageSync('fdbh', '808001');
+    uni.setStorageSync('companyid', '800008');
+    uni.setStorageSync('xsdbh', '');
+    uni.setStorageSync('token', 'XMUGTMwd6RihQZEWBAqvh8OSwLhT95wd');
+
+
+
     const updateManager = uni.getUpdateManager();
     updateManager.onCheckForUpdate(function (res) {
       // 请求完新版本信息的回调
@@ -59,6 +71,84 @@ export default {
           }
         });
       }
+    });
+
+
+    //查询当前桌台订单信息
+    this.$u.api.orders({
+      access_token: uni.getStorageSync('token'),
+      vtype: 'new',
+      tableid: uni.getStorageSync('tableid'),
+      fdbh: uni.getStorageSync('fdbh'),
+      tablewaiter: uni.getStorageSync('syyid'),
+      posid: uni.getStorageSync('posid'),
+      tablenumber: uni.getStorageSync('tablenumber'),
+    }).then((res) => {
+      console.log('查询开台信息', res)
+      //查询桌台订单信息
+      if(res.error_code=='2'){
+        console.log('已有订单')
+        //已开台单号
+        uni.setStorageSync('xsdbh', res.xsdbh)
+        uni.setStorageSync('tablenumber', res.table_number)
+        this.$u.api.orders({
+          access_token: uni.getStorageSync('token'),
+          vtype: 'detail',
+          tableid: uni.getStorageSync('tableid'),
+          fdbh: uni.getStorageSync('fdbh'),
+          xsdbh: uni.getStorageSync('xsdbh')
+        }).then(res => {
+          let old = res;
+          uni.setStorageSync('old', old)
+          console.log('查询桌台订单明细：', res)
+          let cartold = [];
+          let countold=res.count;
+          uni.setStorageSync('flownumold',countold)
+          res.goodslist.forEach((item) => {
+            let choosedText = [];
+            let ext_zxprices = [];
+            item.extlist.forEach(res => {
+              choosedText.push(res.ext_name)
+              ext_zxprices.push(Number.parseInt(res.ext_zxprice))
+            })
+            let text = choosedText.join(',')
+            function sum(arr) {
+              return arr.reduce((prev, curr)=>{
+                return prev + curr;
+              },0);
+            }
+            let addzxprice=sum(ext_zxprices) //属性总价
+
+            cartold.push({
+              id: item.spbm,
+              cate_id: item.category_id,
+              name: item.spmc,
+              number: Number.parseInt(item.quantity) || 1,
+              is_single: item.is_single,
+              choosedText: text || '',
+              price: Number.parseInt(item.price),
+              zxprice: Number.parseInt(addzxprice) + Number.parseInt(item.price),
+              image: `http://cateapi.mzsale.cn/${item.small_img_path}`,
+              addzxprice:addzxprice,
+              goodslist: {
+                discount: item.discount,
+                extlist: item.extlist,
+                flownum: item.flownum,
+                price: Number.parseInt(item.price),
+                zxprice: Number.parseInt(addzxprice) + Number.parseInt(item.price),
+                quantity: Number.parseInt(item.quantity),
+                spbm: item.spbm,
+                spsmm: item.spsmm,
+              }
+            })
+          })
+          console.log('旧数据信息', cartold)
+          uni.setStorageSync('cartold', cartold)
+        })
+      }
+    })
+    uni.switchTab({
+      url: '/pages/main/main'
     });
 
 
